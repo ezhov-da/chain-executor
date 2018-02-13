99805b2fcf1e98043fba2a6e937c3b69dfeeecec
Denis <ezhdenis@yandex.ru>
2018-02-13 17:36:48 +0300

    Update DataSet.java

--- chain-core/src/main/java/ru/ezhov/chain/core/domain/DataSet.java
+++ chain-core/src/main/java/ru/ezhov/chain/core/domain/DataSet.java
@@ -1,20 +1,8 @@
 package ru.ezhov.chain.core.domain;
 
-public class DataSet {
-    private String name;
-    private String version;
-    private String instruction;
-
-    private Object source;
-
-    public DataSet(String name, String version, String instruction, Object source) {
-        this.name = name;
-        this.version = version;
-        this.instruction = instruction;
-        this.source = source;
-    }
-
-    private Object dataSet() {
-        return source;
-    }
+public interface DataSet {
+    void setParam(String param);
+    T getDataSet() throws Exception;
+    String instruction();
+    String version();
 }
