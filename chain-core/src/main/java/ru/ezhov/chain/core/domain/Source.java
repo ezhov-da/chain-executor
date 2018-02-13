059bbfba6695fae69424cbefed6ffd574a22edff
Denis <ezhdenis@yandex.ru>
2018-02-13 17:34:26 +0300

    Update Source.java

--- chain-core/src/main/java/ru/ezhov/chain/core/domain/Source.java
+++ chain-core/src/main/java/ru/ezhov/chain/core/domain/Source.java
@@ -1,26 +1,9 @@
 package ru.ezhov.chain.core.domain;
 
-import ru.ezhov.chain.core.SourceDestroyAction;
-
-public class Source {
-    private String name;
-    private String version;
-    private String instruction;
-
-    private Object source;
-
-    public Source(String name, String version, String instruction, Object source) {
-        this.name = name;
-        this.version = version;
-        this.instruction = instruction;
-        this.source = source;
-    }
-
-    private void preDestroyAction(SourceDestroyAction sourceDestroyAction) {
-        sourceDestroyAction.perform();
-    }
-
-    private Object source() {
-        return source;
-    }
+public interface Source {
+    public void setParam(String param);
+    Object getSource() throws SourceException;
+    void preDestroy();
+    String instruction();
+    String version();
 }
